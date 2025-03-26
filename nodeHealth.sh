b9c955c31039e6f5df0501aed023c57333b9236d
lordemmag-devops <lordemmag@yahoo.com>
2025-03-26 19:44:57 +0100

    additional

---
+++ nodeHealth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##############################
+# Author: Emmanuel
+# Date: 26/03/2025
+#
+# This script outputs the node health
+#
+# Version: v1
+##############################
+
+df -h # Print the disc space.
+
+free -g # Print the memory.
+
+nproc # Printing the resources.

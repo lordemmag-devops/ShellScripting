248d30efd4d05e91d5fed80ec6ce5d1886540e5a
lordemmag-devops <lordemmag@yahoo.com>
2025-04-01 16:43:00 +0100

    added helper function

--- gitHubScript/listAllGithubUsers.sh
+++ gitHubScript/listAllGithubUsers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -35,6 +37,13 @@ function list_users_with_read_access {
 		echo "$collaborators"
 	fi
 }
+
+function helper{
+	expected_cmd_args=2
+	if [$# -ne $expected_cmd_args]; then
+		echo "please execute the script with required cmd args"
+		echo "asd"
+	fi
 
 # Main script
 
